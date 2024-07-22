7d83ba159b0ed9e4739d30a9fadf7fdaa8f6ede4
ankitpawar1 <54860507+ankitpawar1@users.noreply.github.com>
2024-07-22 21:06:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
-const inputs = document.querySelectorAll('.controls input');
+const video = document.querySelector('.video');
+const playerButton = document.querySelector('.player__button');
+const rewindButton = document.querySelector('.rewind');
+const forwardButton = document.querySelector('.forward');
+const volumeControl = document.querySelector('.volume');
+const playbackSpeedControl = document.querySelector('.playbackSpeed');
+const progressBar = document.querySelector('.progress');
+const progressFilled = document.querySelector('.progress__filled');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+const togglePlayPause = () => {
+  if (video.paused) {
+    video.play();
+    playerButton.textContent = '❚ ❚';
+  } else {
+    video.pause();
+    playerButton.textContent = '►';
+  }
+};
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+const updateVolume = () => {
+  video.volume = volumeControl.value;
+};
+
+const updatePlaybackSpeed = () => {
+  video.playbackRate = playbackSpeedControl.value;
+};
+
+const updateProgress = () => {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressFilled.style.width = `${percent}%`;
+};
+
+const rewind = () => {
+  video.currentTime -= 10;
+};
+
+const forward = () => {
+  video.currentTime += 25;
+};
+
+// Event Listeners
+playerButton.addEventListener('click', togglePlayPause);
+rewindButton.addEventListener('click', rewind);
+forwardButton.addEventListener('click', forward);
+volumeControl.addEventListener('input', updateVolume);
+playbackSpeedControl.addEventListener('input', updatePlaybackSpeed);
+video.addEventListener('timeupdate', updateProgress);
